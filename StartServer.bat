d822fecb8bb7a8cb2b017bf506bea28d616eb1f0
Andreas <Holmi1@live.dk>
2021-12-08 22:32:48 +0100

    Clement-08/12/2021-22.32.48,44

---
+++ StartServer.bat
@@ -0,0 +1,5 @@
+git pull
+java -jar -Xms2G -Xmx12G minecraft_server.jar
+git commit -a -m %username%_%date%_%time%
+git push -u origin main
+pause
---
+++ UploadServer.bat
@@ -0,0 +1,3 @@
+git commit -a -m %username%_%date%_%time%
+git push -u origin main
+pause
